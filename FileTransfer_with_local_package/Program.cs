13e9a196f98226c737ec643a76a41313f2d980d0
DmitryLukyanov <dmitry.lukyanov@mongodb.com>
2021-01-06 04:00:32 +0300

    nuget_app

---
+++ FileTransfer_with_local_package/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileTransfer_with_local_package
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = MongoDB.Libmongocrypt.Library.Version;
+        }
+    }
+}
